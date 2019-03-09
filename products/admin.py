f54797e3387e49c255b323fde6a746b8d261494a
Andre Issamu Cavalcanti Mello <aissamu-cavalcanti-m@myseneca.ca>
2019-03-09 16:37:57 -0500

    Added new fields to Composition, 3 new models, CompositionItem, Reference and Type

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Product, Type, Composition
+from .models import Product, Type, Composition, CompositionItem, ReferenceEntity
 
 admin.site.register(Product)
 admin.site.register(Type)
+admin.site.register(CompositionItem)
 admin.site.register(Composition)
+admin.site.register(ReferenceEntity)
--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Type(models.Model):
-    name = models.CharField(max_length = 200)
+    name = models.CharField(max_length = 500)
     
     def __str__(self):
         return self.name
@@ -10,7 +10,7 @@ class Type(models.Model):
         ordering = ('name',)
 
 class Product(models.Model):
-    name = models.CharField(max_length = 200)
+    name = models.CharField(max_length = 500)
     unit = models.CharField(max_length = 20)
     unit_value = models.DecimalField(max_digits = 8, decimal_places=2)
     is_service = models.BooleanField(default=False)
@@ -22,14 +22,34 @@ class Product(models.Model):
     class Meta:
         ordering = ('name',)
          
-class Composition(models.Model):
-    name = models.CharField(max_length = 200)
+class ReferenceEntity(models.Model):
+    name = models.CharField(max_length = 500)
+
+class CompositionItem(models.Model):
+    composition_id = models.CharField(max_length = 20)
+    product_reference_id = models.CharField(max_length = 20)
+    entity = models.ForeignKey(ReferenceEntity, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 500)
+    unit = models.CharField(max_length = 20)
     products = models.ManyToManyField(Product)
+    coeficient = models.DecimalField(max_digits = 8, decimal_places=4)
+    unit_value = Product.unit_value
+
+    def _get_total(self):
+        return self.unit_value * self.coeficient
+
+    total_value = property(_get_total)
 
     def __str__(self):
         return self.name
 
     class Meta:
         ordering = ('name',)
+
+class Composition(models.Model):
+    name = models.CharField(max_length = 500)
+    composition = models.ManyToManyField(CompositionItem)
+
+
 
 
